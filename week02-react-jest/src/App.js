1319aba37143a94c763a96333066b808d2767afe
Your Name <you@example.com>
2018-04-10 21:52:04 -0700

    modified react-jest folder

--- week02-react-jest/src/App.js
+++ week02-react-jest/src/App.js
@@ -3,6 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+	constructor() {
+       super();
+       this.state = {
+		file: 'unknown',
+		foo: 'waiting'
+			};
+		}
+
   render() {
     return (
       <div className="App">
@@ -10,7 +18,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className="App-intro">Nine: {this.state.nine}</p>
+		<button id='getFile' onClick={this.getFile}>Get File</button>
+		<p>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
